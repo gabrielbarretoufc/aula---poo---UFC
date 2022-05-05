71cf94de5d81f159fcadaab2f740e609ae69657c
gabrielbarretoufc <gabrielbarto@alu.ufc.br>
2022-05-05 11:20:01 -0300

    alterando

---
+++ conta/criar_conta.py
@@ -0,0 +1,11 @@
+# criar conta
+
+from src.conta.conta import Conta
+
+class CriarConta:
+    if __name__ == '__main__':
+        conta = Conta('21.324-7')
+        print(conta.get_saldo())
+        conta.creditar(500.87)
+        conta.debitar(45.00)
+        print(conta.get_saldo())
---
+++ conta/criar_poupanca.py
@@ -0,0 +1,15 @@
+# criar poupanca
+
+from src.conta.conta_poupanca import ContaPoupanca
+from src.conta.conta import Conta
+
+class CriarPoupanca:
+    if __name__ == '__main__':
+        conta = Conta("21.342-7")
+        conta = ContaPoupanca(conta)
+        conta.creditar(500.87)
+        conta.debitar(45.00)
+        print(conta.get_saldo())
+
+        conta.render_juros(0.01)
+        print(conta.get_saldo())
